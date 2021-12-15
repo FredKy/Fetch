4dce5d165a9e6d068c0cf3f2aca749338f8c8010
FredKy <pinb4llwiz@protonmail.com>
2021-12-15 20:04:02 +0100

    Update App.js
    Fetch and load test

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = () => {
+    return fetch("https://reactnative.dev/movies.json")
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.movies);
+        console.log(result.movies);
+      })
+      .catch((e) => console.log(e))
+      .finally(setLoading(false));
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      getMovies();
+    }, 1000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {loading ? (
+        <View>
+          <ActivityIndicator size={"large"}></ActivityIndicator>
+          <Text>Waiting for data...</Text>
+        </View>
+      ) : (
+        <View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item, index }) => (
+              <Text>
+                {item.id + ". " + item.title + " (" + item.releaseYear + ")"}
+              </Text>
+            )}
+          />
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
